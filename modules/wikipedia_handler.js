a43bc3416297335c362a91e7f523f6605c0be18c
Joe-Joe-Joe-Joe <joedolina@gmail.com>
2022-11-12 15:23:38 -0500

    add intro message

--- modules/wikipedia_handler.js
+++ modules/wikipedia_handler.js
@@ -4,22 +4,24 @@ const path = require('path');
 let names = ["Joe Rogan", "Joseph Stalin"]
 
 module.exports = {
-    genName: async () => {
+    getSubjectName: async () => {
         //const subj_path = path.join(__dirname, 'subjects.json')
-        namelist = JSON.parse(fs.readFileSync('./subjects.json'));
-        indices = Object.keys(namelist)
-        console.log(indices)
+        //namelist = JSON.parse(fs.readFileSync('./subjects.json'));
+        //indices = Object.keys(namelist)
         //subjNameIndex = indices[Math.floor(Math.random() * indices.length)]
-
+        const subjName = names[Math.floor(Math.random() * names.length)]
+        console.log(subjName)
+        return subjName
     },
 
-    getSubjectName: async (subjectName) => {
+    getWikiDesc: async (subjectName) => {
 	try {
-		const page = await wiki.page('Batman');
-		console.log(page);
+		const page = await wiki.page(subjectName);
+		//console.log(page);
 		//Response of type @Page object
 		const summary = await page.summary();
-		console.log(summary);
+		console.log(summary.extract);
+        return summary.extract
 		//Response of type @wikiSummary - contains the intro and the main image
 	} catch (error) {
 		console.log(error);
