35d79518ac1ae5d1b63bb03a610a4db4d1d6e6f9
Joshua Dietrich <59072117+Taggagii@users.noreply.github.com>
2022-11-12 12:54:58 -0500

    more twitter stuff

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-const twitterInterface = require('./modules/twitterInterface.js');
+const twitterInterface = require("./modules/twitterInterface.js");
 
+// twitterInterface.postTweet('he sighs and then walks home to the other world that is a thing');
+// console.log(twitterInterface.getRecentTweets());
+// const tid = 1591457123275243500;
 
-twitterInterface.postTweet('he sighs and then walks home to the other world that is a thing', ['./tiger.png']);
+// twitterInterface.getReplys(tid);
+
+// twitterInterface.postTweet('testing things')
+
+// console.log(twitterInterface.buildAuthHeader())
+// twitterInterface.printTweet();
+
+// const tid = 1591463308758220801;
+const tid = "1591467058188980224";
+
+const letThing = async () => {
+  // console.log(await twitterInterface.getTweet(tid));
+  console.log(await twitterInterface.getConversation(tid));
+  // await twitterInterface.replyTo('tesitng reply', tid);
+};
+
+letThing();
